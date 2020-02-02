a0aff7bb5d36fed1974da9684c99f66cbd63c5a2
collinlbauer <collinlbauer@gmail.com>
2020-02-02 10:29:22 -0500

    Fix for process names

--- src/collin-grab-procs.c
+++ src/collin-grab-procs.c
@@ -9,86 +9,85 @@ struct process {
     int ppid;                   // parent process ID
     char comm[255];             // name of executable
     unsigned long int vsize;    // size of virtual address space
-    //struct process *next;       // next node in linked list
+    struct process *next;       // next node in linked list
 
 };
 
-// initialize the process structure
-struct process *initProc(char fileName[]) {
-    struct process *proc = malloc(sizeof(struct process));
-    FILE *inFile = fopen(fileName, "r");
-    char dummy[255];
+struct linkedList {
+    struct process *head;
+};
 
-    // test for files not existing.
+// initialize the process structure
+struct process *initProc(char *pid) {
+    // declare process file path
+    char statPath[255];
+    strcpy(statPath, "/proc/");
+    strcat(statPath, pid);
+    strcat(statPath, "/stat");
+    FILE *inFile = fopen(statPath, "r");
+
+    // check if file path exists
     if (inFile == NULL) {
         printf("Error! Could not open file\n");
         exit(-1);
     }
+    
+    struct process *proc = malloc(sizeof(struct process));
+    char dummy[255];
 
-    //printf("Scanning file...");             //debug
-    fscanf(inFile, "%d", &(proc->pid));
-    //printf(" pid <%d>", proc->pid);         //debug
-    fscanf(inFile, "%s", (proc->comm));
-    //printf("comm <%s>", proc->comm);      //debug
-    fscanf(inFile, "%s", dummy);
-    //printf("DUMMY <%d>\n", dummy);      //debug
-    fscanf(inFile, "%d", &(proc->ppid));
-    //printf("ppid <%d>\n", proc->ppid);      //debug
-    for (int i = 0; i < 18; i++) 
+    // scan the file for proc elements
+    fscanf(inFile, "%d", &(proc->pid));     // process ID
+
+    // comm is the process name
+    // This rats nest of code guarantees that the entire name is grabbed,
+    // regardless of spaces. It also removes leading and tailing parenthases.
+    char temp1[255];
+    char temp2[255];
+    fscanf(inFile, "%s", (temp1));     
+    while (temp1[strlen(temp1)-1] != ')') {
+        temp2[0] = '\0';
+        fscanf(inFile, "%s", (temp2));
+        strcat(temp1, " ");
+        strcat(temp1, temp2);
+    }
+    char finalComm[strlen(temp1)-2];
+    for (int i = 0; i < strlen(temp1)-2; i++){
+        finalComm[i] = temp1[i+1];
+    }
+    finalComm[sizeof(finalComm)/sizeof(char)] = '\0';
+    strcpy(proc->comm, finalComm);
+
+    fscanf(inFile, "%s", dummy);            // dummy - skips over single entry
+    fscanf(inFile, "%d", &(proc->ppid));    // Parent process ID
+    for (int i = 0; i < 18; i++)            // dummy - skips over multiple entries
         fscanf(inFile, "%s", dummy);
-    fscanf(inFile, "%ld", &(proc->vsize));
+    fscanf(inFile, "%ld", &(proc->vsize));  // Process vsize
 
+    // close file and return process structure
     fclose(inFile);
-
     return proc;
- 
 };
 
 // build a process structure and print out its status
-int getProcStatus(char *pid) { 
-    // declare process file path
-    char name[255];
-    strcpy(name, "/proc/");
-    strcat(name, pid);
-    strcat(name, "/stat");
-    //printf("name <%s>\n",name);   //debug
-
-    // build process structure
-    struct process* testProc = initProc(name);
+int getProcStatus(struct process *proc) { 
 
     // print process status
-    printf("pid <%d>, ppid <%d>, vsize <%ld>, comm <%s>\n", testProc->pid, testProc->ppid, testProc->vsize, testProc->comm);
+    printf("pid <%d>, ppid <%d>, vsize <%ld>, comm <%s>\n", proc->pid, proc->ppid, proc->vsize, proc->comm);
 
-    free(testProc);
     return 0;
 };
 
-// list the files inside the root folder of a process
-int listProcData(char *name) {
-    struct dirent *de;
-
-    // create path to process
-    char dirname[255];
-    strcpy(dirname, "/proc/");
-    strcat(dirname, name);
-
-    // open process directory
-    DIR *procDir = opendir(dirname);
-    if (procDir == NULL) { // opendir returns NULL if couldn't open directory
-        printf("Could not open current process %s.\n", name);
-        return 0;
+void myFunc(int pid, struct process *proc, int recLayer) {
+    while (proc != NULL) {
+        if (proc->ppid == pid) {
+            for (int i = 0; i < recLayer; i++)
+                printf("  ");
+            printf("(%d) %s, %lu kb\n", proc->pid, proc->comm, (proc->vsize)/1000);
+            myFunc(proc->pid, proc, recLayer + 1);
+        }
+        proc = proc->next;
     }
-
-    // list process contents
-    printf("Process %s contents:\n", name);
-    while ((de = readdir(procDir)) != NULL) {
-        printf("  %s\n", de->d_name);
-    }
-
-    closedir(procDir);
-    return 0;
-};
-
+}
 
 int main(void) {
     struct dirent *de;  // Pointer for directory entry
@@ -109,13 +108,38 @@ int main(void) {
         exit(1);
     }
 
+    struct process *head;
+    struct process *curr;
     // compare regex to each file in directory
     while ((de = readdir(dr)) != NULL) {
         reti = regexec(&regex, de->d_name, 0, NULL, 0);
         if (!reti){ // if file is a process
-            getProcStatus(de->d_name);
+
+            struct process *node = initProc(de->d_name);
+            if (node->pid == 1) {
+                head = node;
+                curr = head;
+            }
+
+            else {
+                while (curr->next != NULL) {
+                    curr = curr->next;
+                }
+
+                curr->next = node;
+            }
+            
         }
     }
+
+    curr = head;
+    //while (curr != NULL) {
+       // getProcStatus(curr);
+     //   curr = curr->next;
+    //}
+
+    myFunc(0, curr, 0);
+
 
     // free memory, close files and end program
     regfree(&regex);
