7dccae5d52669f8f5ecce7217dbf7583a12b383e
collinlbauer <collinlbauer@gmail.com>
2020-04-02 00:25:34 -0400

    word count applied to new method

--- proj3/src/condInt-clb.c
+++ proj3/src/condInt-clb.c
@@ -1,116 +1,95 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
-#include <assert.h>
-#include <string.h>
+#include "./list.c"
 
-#define MAX 256         // acts as max number of lines and max length of single line
-
-char* buffer[MAX];
-int fill_ptr = 0;
-int use_ptr = 0;
-int count = 0;
+// Variable to check that has been entered into the thread.
 int allDone = 0;
 
-void put (char* str) {
-    buffer[fill_ptr] = str;
-    fill_ptr = (fill_ptr + 1) % MAX;
-    count++;
-}
+queue_t q;
 
-char* get() {
-    char* tmp = buffer[use_ptr];
-    use_ptr = (use_ptr + 1) % MAX;
-    count--;
-    return tmp;
-}
-
-pthread_cond_t empty, fill = PTHREAD_COND_INITIALIZER;
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-
-void* producer() {
-    char input[MAX];
-    char* fgot = fgets(input, MAX, stdin);         // first line of file
-    while (fgot != NULL){
-        assert(pthread_mutex_lock(&mutex) == 0);
-        while (count == MAX && allDone == 0) {
-            assert(pthread_cond_wait(&empty, &mutex) == 0);
-        }
-        put(input);
-        char* fgot = fgets(input, MAX, stdin);     // iterates through rest of file
-        if (fgot == NULL) {
-            allDone = 1;
-            assert(pthread_cond_broadcast(&fill) == 0);
-            assert(pthread_mutex_unlock(&mutex) == 0); 
-            break;
-        }
-        assert(pthread_cond_signal(&fill) == 0);
-        assert(pthread_mutex_unlock(&mutex) == 0); 
-    }
-    return NULL;
-}
-
-void* consumer(void *arg) {
+// Thread to pop tasks off queue and process them
+void *consumer(void *arg) {
     int num = (long) arg;
-    while (!allDone || count != 0) {
+    int returnFromDeque = 0;
+    char *tmp = malloc(256);
+    long threadSum = 0;
 
-        assert(pthread_mutex_lock(&mutex) == 0);
-        while (count == 0 && !allDone) {
-            assert(pthread_cond_wait(&fill, &mutex) == 0);
-        }
-        if (allDone && count == 0) {
-            assert(pthread_mutex_unlock(&mutex) == 0);
-            break;
-        }
-        char *tmp = get();
-
-        // word counter loop
-        char currChar = -1;
-        int charNum = 0;
-        int wordCnt = 1;        // This will hella not work for his test cases.
-        while (currChar != 0){
-            currChar = tmp[charNum];
-            if (currChar == ' '){
-                wordCnt++;
-            }
-            charNum++;
-        }
-
-        // line results
-        printf("Thread %d: %s", num, tmp);
-        printf("  %d words.\n", wordCnt);
+    // Note: if returnFromDeque = 0 then the queue was empty
+    while (allDone == 0 || returnFromDeque != -1) {
         
-
-        assert(pthread_cond_signal(&empty) == 0);
-        assert(pthread_mutex_unlock(&mutex) == 0);
+        returnFromDeque = Dequeue(&q, &tmp);
+        
+        //True when thread wasn't empty
+        if (returnFromDeque != -1) {
+            // word counter
+            char currChar = tmp[0];
+            int charNum = 0;
+            int wordCnt = 0;
+            int whiteSpaceFlag = 1;
+            if (currChar != '\n' && currChar != 0 && currChar != ' ') {
+                wordCnt = 1;
+                whiteSpaceFlag = 0;
+            }
+            while (currChar != 0) {
+                currChar = tmp[charNum];
+                if (currChar == ' '){
+                    whiteSpaceFlag = 1;
+                }
+                if (currChar != ' ' && whiteSpaceFlag){
+                    wordCnt++;
+                    whiteSpaceFlag = 0;
+                }
+                charNum++;
+            }
+            printf("Task: %d, Count: %d, Line: %s", num, wordCnt, tmp);
+        }
     }
 
-    return NULL;
+    free(tmp);
+
+    return (void *) threadSum;
+}
+
+void producer() {
+    char input[256];
+    char * fgot = fgets(input, 256, stdin);
+
+    // Note: if the return from fgot was NULL then it read in EOF
+    while (fgot != NULL){
+        Enqueue(&q, input, fgot == NULL);
+        fgot = fgets(input, 256, stdin);
+    }
+    allDone = 1;
 }
 
 int main(int argc, char* argv[]) {
+
     if (argc != 2) {
-        fprintf(stderr, "Number of threads not specified.\n");
-        exit(1);
-    }
-    int numThreads = atoi(argv[1]);
-    if (numThreads <= 0) { 
-        fprintf(stderr, "Number of threads must be a positive integer.\n");
-        exit(1);
+        printf("Incorrect Usage: Please check README\n");
+        exit(-1);
     }
 
+    int numThreads = atoi(argv[1]);
     pthread_t children[numThreads];
+
+    Queue_Init(&q);
+
     for (long i = 0; i < numThreads; i++) {
         assert(pthread_create(&children[i], NULL, consumer, (void *) i) == 0);
     }
 
     producer();
 
+    int totalWords = 0;
 
     for (int i = numThreads - 1; i >= 0; i--) {
-
-        assert(pthread_join(children[i], NULL) == 0);
+        long rValue;
+        assert(pthread_join(children[i], (void **) &rValue) == 0);
+        totalWords += (long) rValue;
     }
+
+    printf("Program Finished. Total Word Count: %d\n", totalWords);
 
 
     return 0;
