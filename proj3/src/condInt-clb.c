c5442ef924c1c414a023f717e365f6d537065d68
collinlbauer <collinlbauer@gmail.com>
2020-04-02 15:59:24 -0400

    comments and some tidying

--- proj3/src/condInt-clb.c
+++ proj3/src/condInt-clb.c
@@ -10,30 +10,37 @@ queue_t q;
 
 // Thread to pop tasks off queue and process them
 void *consumer(void *arg) {
-    int num = (long) arg;
-    int returnFromDeque = 0;
-    char *tmp = malloc(256);
-    long threadSum = 0;
+    int num = (long) arg;           // thread number
+    int returnFromDeque = 0;        // exit condition
+    char *tmp = malloc(256);        // line to read
+    long threadSum = 0;             // total words from thread
 
     // Note: if returnFromDeque = -1 then the queue was empty
     while (allDone == 0 || returnFromDeque != -1) {
-        
         returnFromDeque = Dequeue(&q, &tmp);
         
-        //True when thread wasn't empty
-        if (returnFromDeque != -1) {
-            // word counter
+        if (!returnFromDeque) {     // true when thread wasn't empty
+
+            /* This code count the number of words.
+             * If it sees white space, it sets an internal flag.
+             * If it sees non-whitespace and the flag is set,
+             * it increments the word count and resets the flag.
+             */
             char currChar = tmp[0];
             int charNum = 0;
             int wordCnt = 0;
             int whiteSpaceFlag = 1;
+
+            // check first character
             if (currChar != '\n' && currChar != 0 && currChar != ' ') {
                 wordCnt = 1;
                 whiteSpaceFlag = 0;
             }
+
+            // check each following character
             while (currChar != 0 && currChar != '\n') {
                 currChar = tmp[charNum];
-                if (currChar == ' ' || currChar == '\n'){
+                if (currChar == ' ' || currChar == '\n' || currChar == '\t'){
                     whiteSpaceFlag = 1;
                 }
                 else if (whiteSpaceFlag){
@@ -42,16 +49,18 @@ void *consumer(void *arg) {
                 }
                 charNum++;
             }
+
+            // results from line
             printf("Task: %d, Count: %d, Line: %s", num, wordCnt, tmp);
             threadSum += wordCnt;
         }
     }
 
     free(tmp);
-
     return (void *) threadSum;
 }
 
+// Parent thread that reads and enqueues lines from stdin
 void producer() {
     char input[256];
     char * fgot = fgets(input, 256, stdin);
@@ -65,33 +74,38 @@ void producer() {
 }
 
 int main(int argc, char* argv[]) {
-
     if (argc != 2) {
-        printf("Incorrect Usage: Please check README\n");
+        fprintf(stderr, "Incorrect Usage: Please check README\n");
         exit(-1);
     }
 
+    // read number of threads from arg
     int numThreads = atoi(argv[1]);
+    if (numThreads <= 0){
+        fprintf(stderr, "Incorrect Usage: Please check README\n");
+        exit(-1);
+    }
     pthread_t children[numThreads];
 
     Queue_Init(&q);
 
+    // start consumer threads
     for (long i = 0; i < numThreads; i++) {
         assert(pthread_create(&children[i], NULL, consumer, (void *) i) == 0);
     }
 
+    // producer reads from file and adds to queue
     producer();
 
+    // accumulate each threads' totals
     int totalWords = 0;
-
     for (int i = numThreads - 1; i >= 0; i--) {
         long rValue;
         assert(pthread_join(children[i], (void **) &rValue) == 0);
         totalWords += (long) rValue;
     }
 
+    // results
     printf("Program Finished. Total Word Count: %d\n", totalWords);
-
-
     return 0;
 }
